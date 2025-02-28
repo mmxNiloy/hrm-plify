de677f5e8551288ace2db1a162743979cbd4ea80
Niloy Sarkar <dev.niloysarkar@gmail.com>
2025-03-01 00:34:21 +0600

    8. Restructured the files to remove layout conflict.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-"use client";
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "../globals.css";
@@ -9,7 +8,6 @@ import VersionIndicator from "@/components/custom/VersionIndicator";
 import { Suspense } from "react";
 import Navbar from "@/components/custom/Navbar/Navbar";
 import Footer from "@/components/custom/Footer";
-import { usePathname } from "next/navigation";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -25,8 +23,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const pathname = usePathname();
-
   return (
     <html lang="en">
       <body
@@ -42,13 +38,11 @@ export default function RootLayout({
               nunito.className
             )}
           >
-            {!pathname.startsWith("/dashboard") && <Navbar />}
+            <Navbar />
             {children}
-            {!pathname.startsWith("/dashboard") && (
-              <Suspense fallback={<p>Loading...</p>}>
-                <Footer />
-              </Suspense>
-            )}
+            <Suspense fallback={<p>Loading...</p>}>
+              <Footer />
+            </Suspense>
           </div>
         </Providers>
         {/* <div className="h-screen w-screen top-0 left-0 bottom-0 right-0 sticky bg-green-500/20 -z-10"></div> */}
