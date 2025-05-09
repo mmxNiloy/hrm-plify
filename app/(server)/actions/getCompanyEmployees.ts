abd72e83aa9701d5395c993a4ed6fd64e364b540
Niloy Sarkar <dev.niloysarkar@gmail.com>
2025-05-09 18:00:35 +0600

    Added Ex employee page

--- app/(server)/actions/getCompanyEmployees.ts
+++ app/(server)/actions/getCompanyEmployees.ts
@@ -9,11 +9,13 @@ export async function getCompanyEmployees({
   page,
   limit,
   showMigrantEmployeesOnly,
+  is_active = 1,
 }: {
   companyId: number;
   page: number;
   limit: number;
   showMigrantEmployeesOnly?: boolean;
+  is_active?: 0 | 1;
 }) {
   const sessionId = (await cookies()).get(process.env.COOKIE_SESSION_KEY!);
   const req = fetch(
@@ -21,7 +23,7 @@ export async function getCompanyEmployees({
       process.env.API_BASE_URL
     }/companies/my/get-employee/${companyId}?page=${page}&limit=${limit}&migrants_only=${
       showMigrantEmployeesOnly ? "1" : "0"
-    }`,
+    }&is_active=${is_active}`,
     {
       headers: {
         Authorization: `Bearer ${sessionId?.value}`,
