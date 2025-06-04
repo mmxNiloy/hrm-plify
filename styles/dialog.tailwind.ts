33adbe59e3cb5497109056032db77d32a7609465
Niloy Sarkar <dev.niloysarkar@gmail.com>
2025-06-05 03:21:14 +0600

    Bug fix: Overflow on RTW edit dialog is fixed.

--- styles/dialog.tailwind.ts
+++ styles/dialog.tailwind.ts
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
 
 export const DialogContentWidth = cn(
-  "lg:max-w-2xl xl:max-w-4xl rounded-lg overflow-clip"
+  "max-w-screen-md rounded-lg overflow-clip"
 );
 export const DialogTitleStyles = cn("flex gap-1 items-center");
