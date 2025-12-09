fd23bb10544779c5be404635d5c6d4a84e445890
Niloy Sarkar <dev.niloysarkar@gmail.com>
2025-12-09 18:05:41 +0600

    Change Log: 1. Employee dashboard UI fixes

---
+++ app/(pages)/dashboard/company/[companyId]/employee/(administrative)/@breadcrumbs/staff-report/page.tsx
@@ -0,0 +1,34 @@
+"use server";
+
+import { getCompanyData } from "@/app/(server)/actions/getCompanyData";
+import MyBreadcrumbs from "@/components/custom/Breadcrumbs/MyBreadcrumbs";
+import StaffReportGenerator from "@/components/custom/PDF/StaffReportGenerator";
+import { Skeleton } from "@/components/ui/skeleton";
+import { CompanyIDURLParamSchema } from "@/schema/misc/URLParamSchema";
+import React, { Suspense } from "react";
+
+export default async function StaffReportBreadcrumbs({
+  params,
+}: CompanyIDURLParamSchema) {
+  const mParams = await params;
+  const companyId = mParams.companyId;
+  const company = await getCompanyData(companyId);
+  if (!company.data) return null;
+
+  return (
+    <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-3 sm:gap-4">
+      <Suspense fallback={<Skeleton className="w-3/5 h-10" />}>
+        <MyBreadcrumbs
+          companyId={companyId}
+          parent="Employee Dashboard"
+          title="Staff Report"
+        />
+      </Suspense>
+
+      {/* Download PDF button here */}
+      <div className="w-full sm:w-auto">
+        <StaffReportGenerator company={company.data} />
+      </div>
+    </div>
+  );
+}
